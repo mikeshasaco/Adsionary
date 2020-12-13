4c8c88d0b082b7168b457b6b41e87b104b96da36
Michael Shasanya <shasanyam1@students.rowan.edu>
2020-12-13 08:16:28 -0500

    fixing datatable

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -138,13 +138,154 @@
     border-radius: .25rem;
     }
 
+              .span-test1{
+    color: #fff;
+    background-color:  rgb(136, 23, 241);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
 
 
+            .span-test2{
+    color: #fff;
+    background-color:  rgb(54, 173, 247);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
+
+    
+            .span-test3{
+    color: #fff;
+    background-color:  rgb(138, 248, 151);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
+
+      
+            .span-test4{
+    color: #fff;
+    background-color:  rgb(250, 149, 66);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
+            .span-test5{
+    color: #fff;
+    background-color:  rgb(245, 14, 156);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
+
+            .industry-test1{
+    color: #fff;
+    background-color:  rgb(6, 63, 91);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
+
+            .industry-test2{
+    color: #fff;
+    background-color:  rgb(55, 149, 7);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
+   
+              .industry-test3{
+    color: #fff;
+    background-color:  rgb(26, 4, 96);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
+
+            .industry-test4{
+    color: #fff;
+    background-color:  rgb(95, 22, 9);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
+   
+               .industry-test5{
+    color: #fff;
+    background-color:  rgb(233, 213, 145);
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 90%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+    }
    
 </style>
 
-  <h4 class="text-center">Advertisement Database</h4>    
-    <h6 class="text-center"> <i>(Grow your business, View competitors Advertisements)</i> </h6>    
+  <h5 class="text-center">Database Analytics for Advertisements</h5>    
+    <h6 class="text-center"> <i>(Know what your competitors Advertisements look like)</i> </h6>  
+ <h6 class="text-center"> <i>(Free to Use)</i> </h6>    
+  
 
     
     <div class="container">
@@ -153,14 +294,15 @@
             <table id="example" class="table table-bordered  display nowrap" cellspacing="0" width="100%">
                 <thead>
                     <tr>
-                        <th>Company</th>
-                        <th> Image</th>
+                        <th>Company Name:</th>
+                        <th> Advertisement Image:</th>
                         {{-- <th>Text</th> --}}
-                        <th>Industry</th>
-                        <th> Target Audience <i class="fab fa-facebook-square"></i></th>
-                        <th>Advertisement Type</th>
-                        <th>Percentage Off</th>
-                        <th>Social Media</th>
+                        <th>Industry:</th>
+                        <th> Target Audience:</th>
+                        <th>Advertisement Type:</th>
+                        <th>Percentage Off:</th>
+                        <th>Social Media:</th>
+
 
                     </tr>
                 </thead>
@@ -173,7 +315,18 @@
 
                         {{-- <td>{{$ad->primary}}</td> --}}
 
-                        <td style="font-weight: bold">{{$ad->industry->industryname}}</td>
+                        @if($ad->industry_id == 1)
+                        <td> <span class="industry-test1">{{$ad->industry->industryname}}</span> </td>
+                        @elseif($ad->industry_id == 2)
+                        <td> <span class="industry-test2">{{$ad->industry->industryname}}</span> </td>
+                        @elseif($ad->industry_id == 3)
+                        <td> <span class="industry-test3">{{$ad->industry->industryname}}</span> </td>
+                        @elseif($ad->industry_id == 4)
+                        <td> <span class="industry-test4">{{$ad->industry->industryname}}</span> </td>
+                        @else 
+                         <td> <span class="industry-test5">{{$ad->industry->industryname}}</span> </td>
+                         @endif
+
                         <td>
                              @foreach($ad->tags as $tag)
                              
@@ -183,8 +336,19 @@
                         </td>
 
 
+                        @if($ad->adtype_id == 1)
+                        <td> <span class="span-test1">{{$ad->adtypename}}</span></td>
+                        @elseif($ad->adtype_id == 2)
+                        <td> <span class="span-test2">{{$ad->adtypename}}</span></td>
+                         @elseif($ad->adtype_id == 3)
+                        <td> <span class="span-test3">{{$ad->adtypename}}</span></td>  
+                         @elseif($ad->adtype_id == 4)
+                        <td> <span class="span-test4">{{$ad->adtypename}}</span></td>  
+                        @else
+                        <td> <span class="span-test5">{{$ad->adtypename}}</span></td>  
+                        @endif
 
-                        <td style="font-weight: bold;">{{$ad->adtypename}}</td>
+
 
                     @if(is_null($ad->percentage_id))
                         <td> <span class="span-percentage-na">N/A</span></td>
@@ -194,6 +358,8 @@
                     @else
                           <td> <span class="span-percentage-low">{{$ad->percentnumber }}%</span> </td>
                           @endif
+
+
                        @endif        
                         @if($ad->socialmedia == "Facebook")
                     <td> <span class="span-facebook">{{$ad->socialmedia}} </span> </td>
@@ -209,18 +375,17 @@
                          <td> <span class="span-media">{{$ad->socialmedia}} </span> </td>
                         @endif
 
-
                     </tr>
                     @endforeach
                 </tbody>
                 <tfoot>
                     <tr>
-                        <th>Company</th>
-                        <th>Image</th>
+                        <th>Company Name</th>
+                        <th>Advertisement Image</th>
                         {{-- <th>Text</th> --}}
                         <th>Industry</th>
-                        <th> Target Audience <i class="fab fa-facebook-square"></i></th>
-                        <th>Ad Type</th>
+                        <th> Target Audience</th>
+                        <th>Advertisement Type</th>
                         <th>Percentage Off</th>
                         <th>Social Media</th>
 
