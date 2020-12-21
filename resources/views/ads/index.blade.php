5b598c11a1bfce1f779fa505f84b27ca3e1ad399
Michael Shasanya <shasanyam1@students.rowan.edu>
2020-12-20 21:20:11 -0500

    add enlarge

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -425,16 +425,16 @@
                          {{-- <td> 
                              <img id="myImg"  src="https://adsionary.s3.us-east-2.amazonaws.com/Adsionary/{{$ad->image}}" style="width:100px; height:120px;" alt="{{$ad->primary}}">
                              </td> --}}
-                        <td> 
+                        {{-- <td> 
                           <a href="/images/{{$ad->image}}" data-fancybox="/images/{{$ad->image}}" data-caption="{{$ad->primary}}">
                         <img id="" src="/images/{{$ad->image}}" alt="{{$ad->primary}}" style="width:100px; height:120px;"></a>
-                        <h6 style="color:#5161ce; font-size: 14px; ">Click Enlarge</h6>
-                        </td>
+                        </td> --}}
 
-                         {{-- <td> 
+                         <td> 
                           <a href="https://adsionary.s3.us-east-2.amazonaws.com/Adsionary/{{$ad->image}}"  data-fancybox="{{$ad->image}}" data-caption="{{$ad->primary}}">
                         <img id=""  src="https://adsionary.s3.us-east-2.amazonaws.com/Adsionary/{{$ad->image}}" style="width:100px; height:120px;"></a>
-                        </td> --}}
+                         <h6 style="color:#5161ce; font-size: 13px; ">Click Enlarge</h6>
+                        </td>
 
                          {{-- <td> <img class="data-image-table"   id="data-image"  src="https://vouch.sfo2.digitaloceanspaces.com/home/forge/adsionary.com/storage/app/public/Adsionary/{{$ad->image}}" height="100px" width="100px" alt=""> </td> --}}
 
