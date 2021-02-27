f03d26edd497f77bd5da89e79dcebb6454c3ba28
Michael Shasanya <shasanyam1@students.rowan.edu>
2021-02-26 22:04:31 -0500

    updated the adsionary data

--- resources/views/ads/landingpage.blade.php
+++ resources/views/ads/landingpage.blade.php
@@ -24,6 +24,7 @@
   src="https://www.facebook.com/tr?id=387119268900181&ev=PageView&noscript=1"
 /></noscript> --}}
 
+
 <!-- Facebook Pixel Code -->
 <script>
   !function(f,b,e,v,n,t,s)
@@ -34,13 +35,14 @@
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '803206000541184');
+  fbq('init', '3873690949389526');
   fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=803206000541184&ev=PageView&noscript=1"
+  src="https://www.facebook.com/tr?id=3873690949389526&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
